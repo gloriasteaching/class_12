ae84e1914da022dc1b6a33e4c5ff1b5d58bd5d59
gloriasteaching <47659579+gloriasteaching@users.noreply.github.com>
2022-08-31 13:27:52 +0200

    Cursor

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+game.setDialogCursor(img`
+    . . . . . . . b b . . . . . . . 
+    . . . . . . b d d b . . . . . . 
+    . . . . . b d 5 5 d b . . . . . 
+    . . . . b b 5 5 5 5 b b . . . . 
+    . . . . b 5 5 5 5 5 5 b . . . . 
+    b b b b b 5 5 5 5 1 1 d b b b b 
+    b 5 5 5 5 5 5 5 5 1 1 1 5 5 5 b 
+    b d d 5 5 5 5 5 5 1 1 1 5 d d b 
+    . b d d 5 5 5 5 5 5 5 5 d d b . 
+    . . b b 5 5 5 5 5 5 5 5 b b . . 
+    . . c b 5 5 5 5 5 5 5 5 b c . . 
+    . . c 5 5 5 5 d d 5 5 5 5 c . . 
+    . . c 5 5 d b b b b d 5 5 c . . 
+    . . c 5 d b c c c c b d 5 c . . 
+    . . c c c c . . . . c c c c . . 
+    . . . . . . . . . . . . . . . . 
+    `)
 game.setDialogFrame(img`
     3 3 3 3 3 9 9 9 9 9 3 3 3 3 3 
     3 . . . 3 9 . . . 9 3 . . . 3 
